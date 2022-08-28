34f58eb7313385a872828086bd84c8f7eeba194d
MStewGT <mstewgt@gmail.com>
2022-08-28 10:52:16 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#Retrieve API token from environment variable
+from decouple import config
+OPENAI_TOKEN = config('OPENAI_TOKEN')
+
+#Function to get a random word from an api
+def random_words():
+    import requests
+    url = "https://random-word-api.herokuapp.com/word"
+    response = requests.get(url)
+    data = response.json()
+    word = data[0]
+    return word
+
+#Function to submit the input to the AI
+def submit_to_ai(words):
+    import requests
+    url = 'https://api.openai.com/v1/engines/davinci/completions'
+    headers = {
+        f'Authorization': 'Bearer {OPENAI_TOKEN}',
+    }
+    data = {
+    'prompt': input,
+    'max_tokens': 69,
+    'temperature': 0.7,
+    'top_p': 0.9,
+    'n': 10
+    }
+    response = requests.post(url, headers=headers, json=data)
+    json_response = response.json()
+
+#Get words, generate input, submit to AI
+words = random_words(),random_words()
+input = "What are you thoughts on " + words[0] + " and " + words[1] + "?"
+output = submit_to_ai(input)
+print (output)
